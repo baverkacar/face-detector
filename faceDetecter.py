775f257294d437c1befcab85cadf221d32b55a9b
baverkacar <74879967+baverkacar@users.noreply.github.com>
2021-07-12 14:22:11 +0300

    Comment lines added to main code.

--- faceDetecter.py
+++ faceDetecter.py
@@ -1,22 +1,24 @@
-
-
 import cv2
 import mediapipe as mp
 
-draw = mp.solutions.drawing_utils
-holistic = mp.solutions.holistic
+draw = mp.solutions.drawing_utils # Creating mediapipe drawing variable. It will be draw your face connections.
 
-cap = cv2.VideoCapture(0)
+# MediaPipe Holistic consists of a new pipeline with optimized pose, face and hand components that each run in real-time.
+holistic = mp.solutions.holistic 
+
+# Creating Videocapture Class down below. Parameter 0 is your webcam on computer. If you have another camera you need to enter 1 or 2.
+cap = cv2.VideoCapture(0) 
 
 with holistic.Holistic() as hol:
-    while True:
+    while True: # If you don't stop this program, it will be running with this loop. 
         x, camera = cap.read()
 
-        image = cv2.cvtColor(camera, cv2.COLOR_BGR2RGB)
-        results = hol.process(image)
+        image = cv2.cvtColor(camera, cv2.COLOR_BGR2RGB) # Specifying face connections colors.
+        
+        results = hol.process(image) # Processing face perception.
 
-        draw.draw_landmarks(camera, results.face_landmarks, holistic.FACE_CONNECTIONS)
-        cv2.imshow("baver face detector", camera)
-        cv2.waitKey(2)
+        draw.draw_landmarks(camera, results.face_landmarks, holistic.FACE_CONNECTIONS) # Drawing face connections.
+        cv2.imshow("baver face detector", camera) # Naming program's frame here in this code.
+        cv2.waitKey(2) 
 
 cap.release()
